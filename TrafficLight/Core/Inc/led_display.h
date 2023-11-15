acd5b450b63c96eb5161643bc544695c753b8fec
DESKTOP-D11U3PH\Dell <thinh.nguyenkiengiang0102@hcmut.edu.vn>
2023-11-15 15:47:58 +0700

    led_display

--- TrafficLight/Core/Inc/led_display.h
+++ TrafficLight/Core/Inc/led_display.h
@@ -8,6 +8,12 @@
 #ifndef INC_LED_DISPLAY_H_
 #define INC_LED_DISPLAY_H_
 
+#include "main.h"
+#include "timer.h"
+#include "global.h"
 
+void LedDispMode();
+void LEDScanning();
+void resetCountValue();
 
 #endif /* INC_LED_DISPLAY_H_ */
