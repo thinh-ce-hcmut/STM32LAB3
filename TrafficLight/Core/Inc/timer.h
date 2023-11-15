3f99f0e3d570fcbb071de339647222980f32df47
DESKTOP-D11U3PH\Dell <thinh.nguyenkiengiang0102@hcmut.edu.vn>
2023-11-15 15:15:58 +0700

    Timer

---
+++ TrafficLight/Core/Inc/timer.h
@@ -0,0 +1,24 @@
+/*
+ * timer.h
+ *
+ *  Created on: Nov 15, 2023
+ *      Author: Dell
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+
+
+#endif /* INC_TIMER_H_ */
+
+#define TIMER_CYCLE			10
+
+int get_timer0_flag();
+void setTimer0(int duration);
+int get_timer1_flag();
+void setTimer1(int duration);
+int get_timer2_flag();
+void setTimer2(int duration);
+int get_timer3_flag();
+void setTimer3(int duration);
