5fec7df6462a844ee8f39fc606b1d0dfe48d45f7
DESKTOP-D11U3PH\Dell <thinh.nguyenkiengiang0102@hcmut.edu.vn>
2023-11-15 15:40:14 +0700

    Processing

---
+++ TrafficLight/Core/Inc/processing.h
@@ -0,0 +1,21 @@
+/*
+ * processing.h
+ *
+ *  Created on: Nov 15, 2023
+ *      Author: Dell
+ */
+
+#ifndef INC_PROCESSING_H_
+#define INC_PROCESSING_H_
+
+#include "main.h"
+#include "input_reading.h"
+#include "led_display.h"
+#include "timer.h"
+#include "global.h"
+
+void UpdateMode();
+void LedTimeDurationInit();
+void UpdateDurationValue();
+
+#endif /* INC_PROCESSING_H_ */
