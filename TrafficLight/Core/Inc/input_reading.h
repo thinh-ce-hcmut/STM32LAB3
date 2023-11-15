afdd7b4d8115f865631446df134dbc7a5d06fada
DESKTOP-D11U3PH\Dell <thinh.nguyenkiengiang0102@hcmut.edu.vn>
2023-11-15 15:30:04 +0700

    EX5

---
+++ TrafficLight/Core/Inc/input_reading.h
@@ -0,0 +1,13 @@
+/*
+ * input_reading.h
+ *
+ *  Created on: Nov 15, 2023
+ *      Author: Dell
+ */
+
+#ifndef INC_INPUT_READING_H_
+#define INC_INPUT_READING_H_
+
+
+
+#endif /* INC_INPUT_READING_H_ */
